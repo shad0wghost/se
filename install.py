c9d6006844bbe30805709a5455e6109f59fb9f07
Shad0wGhost <9370866+shad0wghost@users.noreply.github.com>
2019-02-12 00:02:40 -0800

    Update and rename install.sh to install.py

--- install.py
+++ install.py
@@ -1,9 +1,19 @@
-#!/bin/bash
+!#/bin/python
+import os
+import getpass
+
+#install
+os.system("""
 sudo apt-get update
 sudo apt-get install apache2 mysql-server php5 php5-mysql php5-curl pwgen python curl smbclient phpmyadmin -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install paramiko
+""")
+print "Enter MySql Password"
+p = getpass.getpass()
+os.system("sed -i 's/.password.*$/" + p + ";/g' sql.php")
+os.system("""
 php setup.php
 echo '<meta http-equiv="refresh" content="0; url=/se/webfront/scoreboard.php" />' > /var/www/html/index.html
 echo "---------------------------"
@@ -14,3 +24,5 @@ echo "All Set Up! Navigate to http://localhost and access database via http://lo
 echo "---------------------------"
 echo "---------------------------"
 echo "---------------------------"
+echo "---------------------------"
+""")

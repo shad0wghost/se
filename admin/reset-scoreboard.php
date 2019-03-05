148872f9904c8d6aea285e906e06f39a7118264e
Shad0wGhost <9370866+shad0wghost@users.noreply.github.com>
2019-03-04 18:16:37 -0800

    brok

--- admin/reset-scoreboard.php
+++ admin/reset-scoreboard.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-    require('sql.php');
+    require('../sql.php');
     $mysqli = new mysqli($config['host'], $config['user'], $config['pass']);
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
@@ -9,7 +9,7 @@
     $query = "use ccdc"; 
     $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
-    $query = "UPDATE services SET attempts ='0' FROM ccdc"; 
+    $query = "UPDATE services SET attempts ='0'"; 
     $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
     $query = "UPDATE services SET success ='0'";
